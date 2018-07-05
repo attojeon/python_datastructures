5d3ab03719dcf21b6f4edb56af3f6fd27b32cb75
unknown <atto.jeon@gmail.com>
2018-07-06 01:38:50 +0900

    2차원 list를 초기화 하고 입출력 하는 간단한 예제

---
+++ list_1.py
@@ -0,0 +1,23 @@
+##################################################
+# 2차원 배열 사용하는 방법
+###################################################
+
+
+import random
+import sys
+import os
+import pygame as pg 
+
+matrix = []
+onerow = []
+row = 10
+
+for i in range(row):
+    onerow.clear()
+    for j in range(row):
+        onerow.append( random.choice([0,1]) )
+    matrix.append(onerow)
+    onerow = []     # 배열을 초기화해야 합니다. 
+
+for i in range(row):
+    print(matrix[i])
