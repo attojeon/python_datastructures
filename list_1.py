17c37345c5184230ac80e481b2a1885d4572755a
unknown <atto.jeon@gmail.com>
2018-07-09 15:48:59 +0900

    리스트의 사용법 임힘 문제들.

--- list_1.py
+++ list_1.py
@@ -1,5 +1,8 @@
 ##################################################
-# 2차원 배열 사용하는 방법
+# 2차원 리스트 사용하는 방법
+# - 사용자로부터 리스트의 크기를 입력받는다.
+# - 입력받은 리스트의 크기를 이용하여 2차원 리스트를 작성하고 0, 1 중 임의의 수를 세팅한다.
+# - 2차원 리스트를 출력한다.
 ###################################################
 
 
@@ -7,19 +10,24 @@ import random
 import sys
 import os
 import pygame as pg 
+from time import sleep
 
 matrix = []
-onerow = []
-row = 10
+#onerow = []
 
-for i in range(row):
-    onerow.clear()
-    for j in range(row):
+rows = int( input("매트릭스의 크기를 입력하시오>>"))
+print("랜덤한 매트릭스 {}X{}를 작성하고 있습니다.".format(rows, rows))
+print()
+sleep(1)
+
+for i in range(rows):
+    onerow = []
+    for j in range(rows):
         onerow.append( random.choice([0,1]) )
     matrix.append(onerow)
     onerow = []     # 배열을 초기화해야 합니다. 
 
-for i in range(row):
-    print(matrix[i])
 
+for r in matrix:
+    print(r)
 
